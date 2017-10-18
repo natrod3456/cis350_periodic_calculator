97d1bc1d5fdd00b7c9402365ec6f597c5e07193f
natro <natro@DESKTOP-SJUJ7Q1.attlocal.net>
2017-10-17 21:10:09 -0400

    added a feature to see how many times a button is pressed

--- src/cis350/PeriodicCalculator.java
+++ src/cis350/PeriodicCalculator.java
@@ -4,10 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class PeriodicCalculator extends JFrame implements ActionListener{
 	PeriodicTable table;
+	int count = 0;
+	private JButton testButton;
+	
 	public PeriodicCalculator(){
 		table = new PeriodicTable();
 		setLayout(new BorderLayout());
@@ -21,10 +25,17 @@ public class PeriodicCalculator extends JFrame implements ActionListener{
 	}
 	
 	public void actionPerformed(ActionEvent e) {
+		JButton b = (JButton) e.getSource();
 		for (int i = 1; i < 118; i++){
 			if (e.getSource() == table.findButton(i)){
+				if(testButton == b){
 				//mathCalc.addElement(i);
-				System.out.println(i);
+				count++;
+				}
+				else
+					count = 1;
+				System.out.println(i + " pressed " + count + " time(s)!");
+				testButton = b;
 			}
 
 		}
