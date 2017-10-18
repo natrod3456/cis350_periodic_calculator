e11b9c8fd2a73b2b46bcde948fb1c60d97e29ad6
natro <natro@35.40.133.142>
2017-10-18 17:38:57 -0400

    moved file to correct folder

--- src/cis350/MathCalc.java
+++ src/cis350/MathCalc.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class MathCalc {
+	private static final Boolean Fasle = null;
 	ArrayList<Elements> mList;
 	String display;
 	Stack<ArrayList<Elements>> previous;
@@ -11,8 +12,7 @@ public class MathCalc {
 	/******************************************************************
 	 * default constructor for class
 	 *****************************************************************/
-	public MathCalc()
-	{
+	public MathCalc(){
 		mList = new ArrayList<Elements>();
 		display = "";
 		previous = new Stack<ArrayList<Elements>>();
@@ -21,42 +21,32 @@ public class MathCalc {
 	/******************************************************************
 	 * converts the list of elements into a string for display
 	 *****************************************************************/
-	public String toString()
-	{
+	public String toString(){
 		int count = 1;
 		Elements current = null;
 		display = "";
 		
 		// loops through all elements in list
-		for (Elements e : mList)
-		{
+		for (Elements e : mList){
 			// if element is the same as last element, increments count
-			if (e == current && count < 10)
-			{
-				if (display.endsWith(Integer.toString(count)))
-				{
+			if (e == current && count < 10){
+				if (display.endsWith(Integer.toString(count))){
 					display = display.substring(0, display.length() - 1);
 				}
 				count++;
 				display += count;
 			// if element is the same and count is 2 digits, increments count
-			}
-			else if (e == current && count >= 10 && count < 99)
-			{
-				if (display.endsWith(Integer.toString(count)))
-				{
+			}else if (e == current && count >= 10 && count < 99){
+				if (display.endsWith(Integer.toString(count))){
 					display = display.substring(0, display.length() - 2);
 				}
 				count++;
 				display += count;
 				// does nothing if count is > 99
-			}
-			else if (e == current && count > 99)
-			{
+			}else if (e == current && count > 99){
 				
 			}
-			else
-			{
+			else{
 			count = 1;
 			display += e;
 			current = e;
@@ -70,37 +60,51 @@ public class MathCalc {
 	 * adds a new instance of an element to the list
 	 * @param e the element to be added
 	 *****************************************************************/
-	public void addElement(Elements e)
-	{
-		previous.push(mList);
-		mList.add(e);
+	public void addElement(Elements e){
+			ArrayList<Elements> dumb = new ArrayList();
+			for(int i = 0; i < mList.size(); i++) {
+				dumb.add(mList.get(i));
+			}
+			mList.clear();
+			Boolean firstTime = true;
+			for(int i = 0; i < dumb.size(); i++) {
+				if(firstTime && dumb.get(i)==e) {
+					mList.add(e);
+					firstTime = false;
+				}
+				mList.add(dumb.get(i));
+			}
+			if(firstTime) {
+				mList.add(e);
+			}		
 	}
 	
 	/******************************************************************
 	 * removes the last element entered from the list
 	 *****************************************************************/
-	public void removeElement(Elements e)
-	{
-		if (mList.size() != 0)
-		{
+	public void removeElement(){
+		if (mList.size() != 0){
 		mList.remove(mList.size() - 1);
 		}
 	}
 	
-	public void undo()
-	{
-		if (previous.isEmpty())
-		{
+	public void undo(){
+		if (previous.isEmpty()){
 			return;
 		}
 		mList = previous.pop();
 	}
-	
+	/******************************************************************
+	 * removes a new instance of an element to the list
+	 * @param e the element to be removed
+	 *****************************************************************/
+	public void removeElement(Elements e){
+		mList.remove(e);
+	}	
 	/******************************************************************
 	 * clears the entire molecule list
 	 *****************************************************************/
-	public  void clearList()
-	{
+	public  void clearList(){
 		mList.clear();
 	}
 	
@@ -108,11 +112,9 @@ public class MathCalc {
 	 * calculates the atomic weight of the molecule
 	 * @return weight the total weight of the molecule
 	 *****************************************************************/
-	public double getWeight()
-	{
+	public double getWeight(){
 		double weight = 0;
-		for (Elements e : mList)
-		{
+		for (Elements e : mList){
 			weight += e.getAtomicWeight();
 		}
 		return weight;
